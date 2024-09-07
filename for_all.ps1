0a6b6edba82b3eed4a83b1cff4a96a49786ba2e3
Kevin Meinon <kmeinon@gmail.com>
2024-09-07 13:59:17 +0200

    Changed dependencies for ps1

--- for_all.ps1
+++ for_all.ps1
@@ -255,15 +255,8 @@ function InstallMedia {
 }
 
 function InstallDependencies {
-    winget install -h Microsoft.VCRedist.2010.x86 --accept-source-agreements --accept-package-agreements -e
-    winget install -h Microsoft.VCRedist.2012.x64 --accept-source-agreements --accept-package-agreements -e
     winget install -h Microsoft.DotNet.DesktopRuntime.6 --accept-source-agreements --accept-package-agreements -e
-    winget install -h Microsoft.VCRedist.2013.x86 --accept-source-agreements --accept-package-agreements -e
-    winget install -h Microsoft.VCRedist.2013.x64 --accept-source-agreements --accept-package-agreements -e
-    winget install -h Microsoft.VCRedist.2013.x64 --accept-source-agreements --accept-package-agreements -e
-    winget install -h Microsoft.VCRedist.2010.x64 --accept-source-agreements --accept-package-agreements -e
     winget install -h Microsoft.XNARedist --accept-source-agreements --accept-package-agreements -e
-    winget install -h Microsoft.VCRedist.2012.x86 --accept-source-agreements --accept-package-agreements -e
     winget install -h Microsoft.VCRedist.2015+.x86 --accept-source-agreements --accept-package-agreements -e
     winget install -h Microsoft.VCRedist.2015+.x64 --accept-source-agreements --accept-package-agreements -e
 }
